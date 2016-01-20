feb2f1d2e834aed1a58555841f462693018e2d7f
Marco Satti <marcosatti@gmail.com>
2016-01-20 23:55:30 +0800

    Cleanup2

--- Super8_jitcore/Source/Chip8Engine/Chip8Engine_StackHandler.cpp
+++ Super8_jitcore/Source/Chip8Engine/Chip8Engine_StackHandler.cpp
@@ -39,7 +39,9 @@ void Chip8Engine_StackHandler::setTopStack(STACK_ENTRY entry)
 		sp++;
 	}
 	else {
-		//printf("StackHandler:	ERROR! Stack is maxed out (16)! Stack not modified.");
+		char buffer[1000];
+		_snprintf_s(buffer, 1000, "Stack is full! (16 levels). Prepare for a crash or unintended actions! May be a bad rom or emulation bug.");
+		logMessage(LOGLEVEL::L_ERROR, buffer);
 	}
 }
 
